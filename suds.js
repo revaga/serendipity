ccbb3681eafab3cf40b9c2618ff92d4cdb8dc118
revaga <58123400+revaga@users.noreply.github.com>
2024-01-03 20:30:33 -0800

    Update suds.js

--- suds.js
+++ suds.js
@@ -3,9 +3,11 @@ function setup() {
 }
 
 function draw() {
+  if(mouseIsPressed === true){
   rad = random(0,40);
   yval = mouseY + random(-2*rad, 2*rad);
   xval = mouseX + random(-2*rad, 2*rad);
   circle(xval, yval, rad);
+  }
   
 }
